26ddcb8a9ddfb0c1e62e2eeac54ac4f2057ea7cc
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2022-11-22 16:30:23 -0600

    adding comments, testing server and GET route

--- routes/api/inventory-routes.js
+++ routes/api/inventory-routes.js
@@ -4,20 +4,29 @@ const Inventory = require('../../models/Inventory');
 
 // PUT
 
-/*
+
 // GET api/inventory
+// return array of remaining item quantities (an array of integers)
 router.get('/', (req, res) => {
     Inventory.findAll()
     .then(dbInventory => res.json(dbInventory))
-    res.status(204)
+    
     .catch(err => {
         console.log(err);
         res.status(500)
     })
 });
-*/
+
 
 // GET api/inventory/:id
+// return remaining item quantity (an integer)
+router.get('/', (req, res) => {
+    Inventory.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+})
 
 // POST
 
