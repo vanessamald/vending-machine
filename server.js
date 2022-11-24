61b3a991898a64031f4a3851f46717a9314ab644
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2022-11-23 22:53:01 -0600

    adding handlebars, connecting to server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,3 +15,9 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
