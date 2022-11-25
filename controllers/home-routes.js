857b91194a52e6989fb2f7d462424f24ca84e970
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2022-11-25 09:47:32 -0600

    adding homepage routes, connecting db to handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,24 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const  Inventory  = require('../models/Inventory');
 
 router.get('/', (req, res) => {
-    res.render('homepage');
+    Inventory.findAll({
+        attributes: [
+            'id',
+            'name',
+            'price',
+            'quantity'
+        ]
+    }).then(dbInventory => {
+        const drink = dbInventory.map(inventory => inventory.get({plain: true}))
+        res.render('homepage', { drink });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+
 });
 
 module.exports = router;
